1abf718451556437fcd5abbf38664fe6d97059ae
Leslie <leslie.reyes@vtex.com.br>
2022-06-14 14:41:17 +0200

    get list if exist, save in vbase if not

--- node/resolvers/customerClassInfo.ts
+++ node/resolvers/customerClassInfo.ts
@@ -19,26 +19,33 @@ export const customerClassInfo = async (
   const { customerClassValue, url, urlMobile, idImg } = args
   const key = `${customerClassValue}-${idImg}`
 
-  try {
-    const resGetJson: Record<string, unknown> = await vbase.getJSON(
-      BUCKET,
-      CONFIG_PATH
-    )
+  let resGetJson: Record<string, unknown> | null = null
 
+  try {
+    resGetJson = await vbase.getJSON(BUCKET, CONFIG_PATH)
+  } catch (e) {
+    console.info('error:', e)
+  }
+
+  if (!resGetJson) {
+    const customerUrls = {
+      [key]: { url, urlMobile },
+    }
+
+    console.info('customer-imgId: urls: ', customerUrls)
+    const resSaveJson = await vbase.saveJSON(BUCKET, CONFIG_PATH, customerUrls)
+
+    console.info('list does not exist, resSaveJson', resSaveJson)
+  } else {
     resGetJson[key] = { url, urlMobile }
     const resSaveJson = await vbase.saveJSON(BUCKET, CONFIG_PATH, resGetJson)
-    // eslint-disable-next-line no-console
 
     console.info('savedJSON res: ', resSaveJson)
 
     const resGetJsonAfter = await vbase.getJSON(BUCKET, CONFIG_PATH)
 
-    // eslint-disable-next-line no-console
-
     console.info('getJSON res: ', resGetJsonAfter)
 
     return args
-  } catch (e) {
-    return e
   }
 }

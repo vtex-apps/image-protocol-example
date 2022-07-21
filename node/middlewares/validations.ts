272e22c47f341b594f20ad524e101d0b01d8a340
Leslie <leslie.reyes@vtex.com.br>
2022-07-21 15:28:20 +0200

    validations.ts

--- node/middlewares/validations.ts
+++ node/middlewares/validations.ts
@@ -5,6 +5,20 @@ export async function validations(ctx: Context, next: () => Promise<unknown>) {
 
   const { fields, files } = await asyncBusboy(req)
 
+  if (
+    (fields.imgId.length === 0 && !files && fields.hrefImg.length === 0) ||
+    fields.imgId.length === 0 ||
+    !files ||
+    fields.hrefImg.length === 0 ||
+    (fields.customerClass.trim().length === 0 &&
+      fields.polygon.trim().length === 0)
+  ) {
+    ctx.status = 400
+    ctx.body = 'Required data missing'
+
+    return
+  }
+
   const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/gm
   const regExp = new RegExp(expression)
 
@@ -22,19 +36,6 @@ export async function validations(ctx: Context, next: () => Promise<unknown>) {
     ctx.status = 400
     ctx.body =
       'Wrong format for customerClass and/or polygon. Only letters and numbers are allowed'
-
-    return
-  }
-
-  if (
-    (fields.imgId.length === 0 && !files && fields.hrefImg.length === 0) ||
-    fields.imgId.length === 0 ||
-    !files ||
-    fields.hrefImg.length === 0 ||
-    (fields.customerClass.length === 0 && fields.polygon.length === 0)
-  ) {
-    ctx.status = 400
-    ctx.body = 'Required data missing'
 
     return
   }

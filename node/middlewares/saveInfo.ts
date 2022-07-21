3f9eec0cad77d2601300e816047044c1aca6e6b8
Leslie <leslie.reyes@vtex.com.br>
2022-07-21 16:40:39 +0200

    logger added in saveInfo middleware

--- node/middlewares/saveInfo.ts
+++ node/middlewares/saveInfo.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
-
 import type { ReadStream } from 'fs'
+
+import { LogLevel } from '@vtex/api'
 
 import {
   BUCKET,
@@ -30,6 +31,7 @@ interface ObjLinks {
 export async function saveInfo(ctx: Context) {
   const {
     clients: { fileManager, vbase },
+    vtex: { logger },
   } = ctx
 
   const { fields, files } = ctx.state.request
@@ -66,17 +68,34 @@ export async function saveInfo(ctx: Context) {
 
   const { url, urlMobile } = objLinks
 
-  if (customerClass.trim().length > 0 && polygon.trim().length > 0) {
+  if (customerClass.length > 0 && polygon.length > 0) {
     try {
       key = `${customerClass}-${polygon}-${imgId}`
       getCustomerList = await vbase.getJSON(BUCKET, CONFIG_PATH_CCPOLYGON, true)
-
+      logger.log(
+        {
+          message: `saveInfo middleware. If customerClass and polygon exist. Inside try block. Get saved data in vbase`,
+          detail: {
+            getCustomerList,
+          },
+        },
+        LogLevel.Info
+      )
       if (!getCustomerList) {
         const customerUrls = {
           [key]: { url, urlMobile, hrefImg },
         }
 
         await vbase.saveJSON(BUCKET, CONFIG_PATH_CCPOLYGON, customerUrls)
+        logger.log(
+          {
+            message: `saveInfo middleware. If customerClass and polygon exist and there is no data saved in vbase yet`,
+            detail: {
+              customerUrls,
+            },
+          },
+          LogLevel.Info
+        )
 
         ctx.status = 201
         ctx.body = customerUrls
@@ -84,6 +103,17 @@ export async function saveInfo(ctx: Context) {
         getCustomerList[key] = { url, urlMobile, hrefImg }
         await vbase.saveJSON(BUCKET, CONFIG_PATH_CCPOLYGON, getCustomerList)
 
+        logger.log(
+          {
+            message: `saveInfo middleware. If customerClass and polygon exist and there is data saved in vbase. Update record ${getCustomerList[key]}`,
+            detail: {
+              url,
+              urlMobile,
+              hrefImg,
+            },
+          },
+          LogLevel.Info
+        )
         await vbase.getJSON(BUCKET, CONFIG_PATH_CCPOLYGON)
 
         ctx.status = 201
@@ -92,17 +122,31 @@ export async function saveInfo(ctx: Context) {
     } catch (e) {
       const response = { url: null, urlMobile: null, hrefImg: null }
 
+      logger.log(
+        {
+          message: `saveInfo middleware. Catch block`,
+          detail: {
+            e,
+          },
+        },
+        LogLevel.Error
+      )
       ctx.status = 404
       ctx.body = response
     }
-  } else if (
-    customerClass.trim().length > 0 &&
-    (!polygon || polygon.trim().length === 0)
-  ) {
+  } else if (customerClass.length > 0 && (!polygon || polygon.length === 0)) {
     try {
       key = `${customerClass}-${imgId}`
       getCustomerList = await vbase.getJSON(BUCKET, CONFIG_PATH_CC, true)
-
+      logger.log(
+        {
+          message: `saveInfo middleware. Inside try block. If only customerClass exist. Get saved data in vbase`,
+          detail: {
+            getCustomerList,
+          },
+        },
+        LogLevel.Info
+      )
       if (!getCustomerList) {
         const customerUrls = {
           [key]: { url, urlMobile, hrefImg },
@@ -110,48 +154,111 @@ export async function saveInfo(ctx: Context) {
 
         await vbase.saveJSON(BUCKET, CONFIG_PATH_CC, customerUrls)
 
+        logger.log(
+          {
+            message: `saveInfo middleware. If only customerClass exist and there is no data saved in vbase yet`,
+            detail: {
+              customerUrls,
+            },
+          },
+          LogLevel.Info
+        )
         ctx.status = 201
         ctx.body = customerUrls
       } else {
         getCustomerList[key] = { url, urlMobile, hrefImg }
         await vbase.saveJSON(BUCKET, CONFIG_PATH_CC, getCustomerList)
-
+        logger.log(
+          {
+            message: `saveInfo middleware. If only customerClass exist and there is data saved in vbase. Update record ${getCustomerList[key]}`,
+            detail: {
+              url,
+              urlMobile,
+              hrefImg,
+            },
+          },
+          LogLevel.Info
+        )
         ctx.status = 201
         ctx.body = getCustomerList[key]
       }
     } catch (e) {
       const response = { url: null, urlMobile: null, hrefImg: null }
 
+      logger.log(
+        {
+          message: `saveInfo middleware. Inside the catch block`,
+          detail: {
+            e,
+          },
+        },
+        LogLevel.Error
+      )
       ctx.status = 404
       ctx.body = response
     }
   } else if (
-    (!customerClass || customerClass.trim().length === 0) &&
+    (!customerClass || customerClass.length === 0) &&
     polygon.trim().length > 1
   ) {
     try {
       key = `${polygon}-${imgId}`
       getCustomerList = await vbase.getJSON(BUCKET, CONFIG_PATH_POLYGON, true)
-
+      logger.log(
+        {
+          message: `saveInfo middleware. If only polygon exists. Get saved data in vbase`,
+          detail: {
+            getCustomerList,
+          },
+        },
+        LogLevel.Info
+      )
       if (!getCustomerList) {
         const customerUrls = {
           [key]: { url, urlMobile, hrefImg },
         }
 
         await vbase.saveJSON(BUCKET, CONFIG_PATH_POLYGON, customerUrls)
-
+        logger.log(
+          {
+            message: `saveInfo middleware. If only polygon exists. No data saved in vbase yet.`,
+            detail: {
+              getCustomerList,
+            },
+          },
+          LogLevel.Info
+        )
         ctx.status = 201
         ctx.body = customerUrls
       } else {
         getCustomerList[key] = { url, urlMobile, hrefImg }
         await vbase.saveJSON(BUCKET, CONFIG_PATH_POLYGON, getCustomerList)
-
+        logger.log(
+          {
+            message: `saveInfo middleware. If only polygon exists. Data saved in vbase. Update record ${getCustomerList[key]}`,
+            detail: {
+              url,
+              urlMobile,
+              hrefImg,
+            },
+          },
+          LogLevel.Info
+        )
         ctx.status = 201
         ctx.body = getCustomerList[key]
       }
     } catch (e) {
       const response = { url: null, urlMobile: null, hrefImg: null }
 
+      logger.log(
+        {
+          message: `saveInfo middleware. If only polygon exists. Inside catch block.`,
+          detail: {
+            e,
+          },
+        },
+        LogLevel.Error
+      )
       ctx.status = 404
       ctx.body = response
     }
